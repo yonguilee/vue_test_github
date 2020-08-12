b653876460223a36c992a2206dd8e5eb821147cd
yongui <ye2438@gmail.com>
2020-08-12 23:07:57 +0900

    This is works

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  },
+  {
+    path: "/",
+    name: "catchAll",
+    component: Home
+  },
 ];
 
 const router = new VueRouter({
